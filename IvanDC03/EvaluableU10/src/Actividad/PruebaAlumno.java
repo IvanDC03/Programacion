980c5523a26cabe285588da750c1677b90660f88
IvanDC03 <ivandiezcardenas32@gmail.com>
2023-02-26 12:10:06 +0100

    "Ultimos Cambios"

--- IvanDC03/EvaluableU10/src/Actividad/PruebaAlumno.java
+++ IvanDC03/EvaluableU10/src/Actividad/PruebaAlumno.java
@@ -5,7 +5,7 @@ public class PruebaAlumno {
 	public static void main(String[] args) {
 //Instanciacion de tres objetos de tipo Alumno
 		Alumno alumno1 = new Alumno("Juan", "Segundo", 7);
-		Alumno alumno2 = new Alumno("Alberto", "Tercero");
+		Alumno alumno2 = new Alumno("Alberto","Tercero");
 		Alumno alumno3 = new Alumno("Alejandro");
 	}
 
