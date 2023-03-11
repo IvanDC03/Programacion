fb4260aee5277de8b367781dafdb1cc92bbf6d53
IvanDC03 <ivandiezcardenas32@gmail.com>
2023-03-11 21:07:00 +0100

    commit

--- IvanDC03/Supermercado2/src/Supermercado/Supermercado.java
+++ IvanDC03/Supermercado2/src/Supermercado/Supermercado.java
@@ -3,12 +3,81 @@ package Supermercado;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Supermercado {
-
-List<String> productos = Arrays.asList("Tomate","Aceite","Pan","Leche","Yogur","Queso","Jamon","Harina","Champu","Manzana");
-
-
-
+private String productos;
+String productos1;
+ArrayList<String> cliente = new ArrayList<>();
+public static int opciones(int opcion,String productos1) {
+	if (opcion==1) {
+		System.out.println(productos1);
+	}
+	//Añadir productos al supermercado
+	ArrayList<String> productos = new ArrayList<>(productos);
+	if (opcion==2) {
+productos.add(productos1);
+	}
+	//Mostrar carrito 
+	else if (opcion==3) {
+	productos.sort((Comparator<? super String>) productos);
+		System.out.println(productos.toString());
+	}
+	//Consultar producto
+	else if (opcion==4) {
+		
+	}
+	//Cambiar producto
+	else if (opcion==5) {
+		
+	}
+	//Salir 
+	else if (opcion==6) {
+		String salir = " ";
+		System.out.println("Salir" + salir);
+	}
+	return opcion;
 }
+public static int menu(int menu,String productos1,String cliente) {
+//Funcionalidad del menu 
+	switch (menu) {
+	case 1:
+	    Collections.sort(productos1);
+		System.out.println(productos1.toString());
+		break;
+	case 2:
+		
+		break;
+	case 3:
+		Collections.sort(productos1);
+		System.out.println(cliente.toString());
+		break;	
+	case 4:
+		cliente.
+		break;	
+	case 5:
+		
+		break;
+	case 6:
+		System.out.println("Fin");
+		break;
+	default:
+	String salir1 = " ";
+	System.out.println("Salir" + salir1);
+		break;
+	}
+}
+//Nueva forma de mostrar el menu
+public int acceso(int opcionAcesso) {
+	
+	if (opcionAcesso==1) {
+		System.out.println();
+	}
+	else if (opcionAcesso==2) {
+		System.out.println();
+	}
+	return opcionAcesso;
+}
+}
+
--- IvanDC03/Supermercado2/src/Supermercado/SupermercadoMain.java
+++ IvanDC03/Supermercado2/src/Supermercado/SupermercadoMain.java
@@ -20,64 +20,10 @@ System.out.println("3. Mostrar carrito de la compra ordenado");
 System.out.println("4. Consultar productos del carrito");
 System.out.println("5. Cambiar un producto por otro");
 System.out.println("6. Salir");
-//Productos del supermercado y inicializados con una lista 
+//Productos del supermercado
 SupermercadoEnum.values();
-if (opcion==1) {
-	System.out.println(productos);
-}
-//Añadir productos al supermercado
-ArrayList<String> productos1 = new ArrayList<>(productos);
-if (opcion==2) {
-	productos1.addAll(productos);
-}
-//Mostrar carrito 
-else if (opcion==3) {
-	Collections.sort(productos);
-	System.out.println(productos.toString());
-}
-//Consultar producto
-else if (opcion==4) {
-	
-}
-//Cambiar producto
-else if (opcion==5) {
-	
-}
-//Salir 
-else if (opcion==6) {
-	String salir = " ";
-	System.out.println("Salir" + salir);
-}
-//Productos que el cliente a seleccionado
+//Productos que el cliente a seleccionado y guardados en una lista
 ArrayList<String> cliente = new ArrayList<>();
-//Funcionalidad del menu 
-int menu = 0;
-switch (menu) {
-case 1:
-    Collections.sort(productos);
-	System.out.println(productos.toString());
-	break;
-case 2:
-	
-	break;
-case 3:
-	Collections.sort(productos);
-	System.out.println(cliente.toString());
-	break;	
-case 4:
-	cliente.addAll(productos);
-	break;	
-case 5:
-	
-	break;
-case 6:
-	System.out.println("Fin");
-	break;
-default:
-String salir1 = " ";
-System.out.println("Salir" + salir1);
-	break;
-}	
 //Nueva forma de mostrar el menu
 System.out.println("Introduce una opcion: ");
 int opcionAcesso = sc.nextInt();
@@ -88,7 +34,7 @@ if (opcionAcesso==1) {
 	System.out.println();
 }
 else if (opcionAcesso==2) {
-	System.out.println("Cliente");
+	System.out.println();
 }
 //Menu login 
 System.out.println("Introduzca usuario y contraseña: ");
@@ -100,6 +46,8 @@ System.out.println("2. Añadir productos");
 System.out.println("3. Eliminar producto");
 System.out.println("4. Modificar producto");
 System.out.println("5. Salir");
+//Llamar funciones 
+
 //Menu del cliente
 System.out.println("Elije una opcion: ");
 int opcion4 = sc.nextInt();
