678c63634e57e7badf1efaaa9d5a1f345704379a
IvanDC03 <ivandiezcardenas32@gmail.com>
2023-03-22 14:34:45 +0100

    commit

--- Evaluable12/src/Actividad/EmpleadoProduccion.java
+++ Evaluable12/src/Actividad/EmpleadoProduccion.java
@@ -2,10 +2,6 @@ package Actividad;
 
 public class EmpleadoProduccion extends Asalariado {
 
-	public EmpleadoProduccion(String nombre, long dni, int numDias) {
-		super(nombre, dni, numDias);
-		// TODO Auto-generated constructor stub
-	}
-
+	
 
 }
