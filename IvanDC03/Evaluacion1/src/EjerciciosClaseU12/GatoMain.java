e9279b86d3113485b8901f6cb7639ff9a42373c6
IvanDC03 <ivandiezcardenas32@gmail.com>
2023-03-23 13:22:38 +0100

    commit

--- IvanDC03/Evaluacion1/src/EjerciciosClaseU12/GatoMain.java
+++ IvanDC03/Evaluacion1/src/EjerciciosClaseU12/GatoMain.java
@@ -6,7 +6,7 @@ import java.util.Collections;
 public class GatoMain {
 
 	public static void main(String[] args) {
-ArrayList<Gato> gatos1 = new ArrayList<>();
+/*ArrayList<Gato> gatos1 = new ArrayList<>();
 gatos1.add("Lucia",2,3755);
 gatos1.add("Alberto",3,3423);
 gatos1.add("Antonio",6,7874);
@@ -14,7 +14,7 @@ gatos1.add("Jorge",7,8745);
 gatos1.add("Juan",8,4587);
 gatos1.add("Alvaro",6,1564);
 System.out.println(gatos1.toString());
-Collections.sort(gatos);
+Collections.sort(gatos);*/
 
 
 	}
