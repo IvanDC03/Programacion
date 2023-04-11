7a4aaace82ae7bf33962482350df77cdaf7ad492
IvanDC03 <ivandiezcardenas32@gmail.com>
2023-04-11 13:08:45 +0200

    commit

---
+++ progrmacion23/Evaluacion1/src/EjerciciosClaseU14/EntradaDatos.java
@@ -0,0 +1,16 @@
+package EjerciciosClaseU14;
+
+import java.util.Scanner;
+
+public class EntradaDatos {
+	private String nombre;
+	private int edad;
+	public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+public static String PedirDatos (){
+	
+}
+
+	}
+
+}
