838e8951f0bcbd71b473038e5e46ad1e950abd95
IvanDC03 <ivandiezcardenas32@gmail.com>
2023-04-27 14:38:39 +0200

    commit

--- progrmacion23/Evaluacion1/src/EjerciciosClaseU15/Calculadora/Calculadora.java
+++ progrmacion23/Evaluacion1/src/EjerciciosClaseU15/Calculadora/Calculadora.java
@@ -5,26 +5,29 @@ import javax.swing.*;
 public class Calculadora {
 
 	public static void main(String[] args) {
-Calcula calculo = new Calcula();
-BotonesAsignacion botones1 = new BotonesAsignacion();
-BotonesNumericos botones2 = new BotonesNumericos();
+		Calcula calculo = new Calcula();
+		BotonesAsignacion botones1 = new BotonesAsignacion();
+		BotonesNumericos botones2 = new BotonesNumericos();
 	}
 }
-class Calcula extends JFrame{
-public Calcula() {
-	setTitle("Calculadora Basica");
-	setBounds(300,300,450,300);
-	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	setVisible(true);
-	
+
+class Calcula extends JFrame {
+	public Calcula() {
+		setTitle("Calculadora Basica");
+		setBounds(300, 300, 450, 300);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+
+	}
 }
-}
-class BotonesAsignacion extends JPanel{
+
+class BotonesAsignacion extends JPanel {
 	private JButton mas = new JButton("+");
 	private JButton menos = new JButton("-");
 	private JButton multiplicar = new JButton("*");
 	private JButton dividir = new JButton("/");
 	private JButton borrar = new JButton("C");
+
 	public BotonesAsignacion() {
 		add(mas);
 		add(menos);
@@ -33,7 +36,8 @@ class BotonesAsignacion extends JPanel{
 		add(borrar);
 	}
 }
-class BotonesNumericos extends JPanel{
+
+class BotonesNumericos extends JPanel {
 	private JButton uno = new JButton("1");
 	private JButton dos = new JButton("2");
 	private JButton tres = new JButton("3");
@@ -43,6 +47,7 @@ class BotonesNumericos extends JPanel{
 	private JButton siete = new JButton("7");
 	private JButton ocho = new JButton("8");
 	private JButton nueve = new JButton("9");
+
 	public BotonesNumericos() {
 		add(uno);
 		add(dos);
