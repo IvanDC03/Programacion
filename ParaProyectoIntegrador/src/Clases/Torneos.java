d6eb7c31ef6448e27db3eb4285272e1e70502658
IvanDC03 <ivandiezcardenas32@gmail.com>
2023-05-21 18:34:21 +0200

    commit

--- ParaProyectoIntegrador/src/Clases/Torneos.java
+++ ParaProyectoIntegrador/src/Clases/Torneos.java
@@ -6,10 +6,15 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
+import javax.swing.AbstractAction;
+import java.awt.event.ActionEvent;
+import javax.swing.Action;
+import java.awt.Color;
 
 public class Torneos extends JFrame {
 
 	private JPanel contentPane;
+	
 
 	/**
 	 * Launch the application.
@@ -31,6 +36,7 @@ public class Torneos extends JFrame {
 	 * Create the frame.
 	 */
 	public Torneos() {
+		setTitle("Torneos");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 590, 400);
 		contentPane = new JPanel();
@@ -39,44 +45,57 @@ public class Torneos extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JButton btnNewButton_1 = new JButton("New button");
-		btnNewButton_1.setBounds(361, 69, 85, 21);
+		JButton btnNewButton_1 = new JButton("Boton 1");
+		btnNewButton_1.setForeground(Color.ORANGE);
+		btnNewButton_1.setBounds(20, 69, 85, 21);
 		contentPane.add(btnNewButton_1);
-		
-		JButton btnNewButton = new JButton("New button");
-		btnNewButton.setBounds(191, 173, 85, 21);
+		btnNewButton_1.setBackground(Color.BLUE);
+		JButton btnNewButton = new JButton("Boton 2");
+		btnNewButton.setForeground(Color.ORANGE);
+		btnNewButton.setBounds(242, 69, 85, 21);
 		contentPane.add(btnNewButton);
-		
-		JButton btnNewButton_2 = new JButton("New button");
-		btnNewButton_2.setBounds(20, 69, 85, 21);
-		contentPane.add(btnNewButton_2);
-		
-		JButton btnNewButton_3 = new JButton("New button");
-		btnNewButton_3.setBounds(20, 123, 85, 21);
+		btnNewButton.setBackground(Color.BLUE);
+		JButton btnNewButton_3 = new JButton("Boton 3");
+		btnNewButton_3.setForeground(Color.ORANGE);
+		btnNewButton_3.setBounds(481, 69, 85, 21);
 		contentPane.add(btnNewButton_3);
-		
-		JButton btnNewButton_4 = new JButton("New button");
-		btnNewButton_4.setBounds(20, 173, 85, 21);
+		btnNewButton_3.setBackground(Color.BLUE);
+		JButton btnNewButton_4 = new JButton("Boton 4");
+		btnNewButton_4.setForeground(Color.ORANGE);
+		btnNewButton_4.setBounds(20, 123, 85, 21);
 		contentPane.add(btnNewButton_4);
-		
-		JButton btnNewButton_5 = new JButton("New button");
-		btnNewButton_5.setBounds(191, 217, 85, 21);
+		btnNewButton_4.setBackground(Color.BLUE);
+		JButton btnNewButton_5 = new JButton("Boton 5");
+		btnNewButton_5.setForeground(Color.ORANGE);
+		btnNewButton_5.setBounds(242, 123, 85, 21);
 		contentPane.add(btnNewButton_5);
-		
-		JButton btnNewButton_6 = new JButton("New button");
-		btnNewButton_6.setBounds(191, 69, 85, 21);
+		btnNewButton_5.setBackground(Color.BLUE);
+		JButton btnNewButton_6 = new JButton("Boton 6");
+		btnNewButton_6.setForeground(Color.ORANGE);
+		btnNewButton_6.setBounds(481, 123, 85, 21);
 		contentPane.add(btnNewButton_6);
-		
-		JButton btnNewButton_7 = new JButton("New button");
-		btnNewButton_7.setBounds(191, 123, 85, 21);
+		btnNewButton_6.setBackground(Color.BLUE);
+		JButton btnNewButton_7 = new JButton("Boton 7");
+		btnNewButton_7.setForeground(Color.ORANGE);
+		btnNewButton_7.setBounds(20, 179, 85, 21);
 		contentPane.add(btnNewButton_7);
-		
-		JButton btnNewButton_8 = new JButton("New button");
-		btnNewButton_8.setBounds(361, 123, 85, 21);
+		btnNewButton_7.setBackground(Color.BLUE);
+		JButton btnNewButton_8 = new JButton("Boton 8");
+		btnNewButton_8.setForeground(Color.ORANGE);
+		btnNewButton_8.setBounds(242, 179, 85, 21);
 		contentPane.add(btnNewButton_8);
-		
-		JButton btnNewButton_9 = new JButton("New button");
-		btnNewButton_9.setBounds(361, 173, 85, 21);
+		btnNewButton_8.setBackground(Color.BLUE);
+		JButton btnNewButton_9 = new JButton("Boton 9");
+		btnNewButton_9.setForeground(Color.ORANGE);
+		btnNewButton_9.setBounds(481, 179, 85, 21);
 		contentPane.add(btnNewButton_9);
+		btnNewButton_9.setBackground(Color.BLUE);
+		JButton btnNewButton_3_1 = new JButton("Boton 10");
+		btnNewButton_3_1.setForeground(Color.ORANGE);
+		btnNewButton_3_1.setBounds(242, 233, 85, 21);
+		contentPane.add(btnNewButton_3_1);
+		btnNewButton_3_1.setBackground(Color.BLUE);
 	}
-}
+
+	}
+
--- ParaProyectoIntegrador/src/Clases/TorneosDentro.java
+++ ParaProyectoIntegrador/src/Clases/TorneosDentro.java
@@ -1,5 +1,6 @@
 package Clases;
 
+import java.awt.Color;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
@@ -10,11 +11,16 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JTable;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.KeyStroke;
+import java.awt.event.KeyEvent;
 
 public class TorneosDentro extends JFrame {
 
 	private JPanel contentPane;
 	private JTable table;
+	
 
 	/**
 	 * Launch the application.
@@ -25,6 +31,7 @@ public class TorneosDentro extends JFrame {
 				try {
 					TorneosDentro frame = new TorneosDentro();
 					frame.setVisible(true);
+					frame.setBackground(Color.CYAN);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -36,39 +43,44 @@ public class TorneosDentro extends JFrame {
 	 * Create the frame.
 	 */
 	public TorneosDentro() {
+		setBackground(Color.WHITE);
+		setTitle("Parte interna de Torneos");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 578, 387);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-
+		
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JButton btnNewButton = new JButton("Atras");
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		btnNewButton.setBounds(20, 28, 110, 35);
-		contentPane.add(btnNewButton);
-		
-		JButton btnNewButton_1 = new JButton("New button");
+		JButton btnNewButton_1 = new JButton("Boton 1");
 		btnNewButton_1.setBounds(34, 99, 85, 21);
 		contentPane.add(btnNewButton_1);
-		
-		JButton btnNewButton_2 = new JButton("New button");
+		btnNewButton_1.setBackground(Color.BLUE);
+		JButton btnNewButton_2 = new JButton("Boton 2");
 		btnNewButton_2.setBounds(209, 99, 85, 21);
 		contentPane.add(btnNewButton_2);
-		
-		JButton btnNewButton_3 = new JButton("New button");
+		btnNewButton_2.setBackground(Color.BLUE);
+		JButton btnNewButton_3 = new JButton("Boton 3");
 		btnNewButton_3.setBounds(389, 99, 85, 21);
 		contentPane.add(btnNewButton_3);
+		btnNewButton_3.setBackground(Color.BLUE);
 		String[] columnNames = {"Nombre", "Posición", "Puntos", "Edad", "Nacionalidad"};
 		Object[][] data = new Object[20][5];
-		
 				JTable table_1 = new JTable(data, columnNames);
-				table_1.setBounds(44, 155, 466, 169);
+				table_1.setBounds(34, 151, 466, 162);
 				contentPane.add(table_1);
+				table_1.setBackground(Color.WHITE);
+				table_1.setVisible(true);
+				JButton btnNewButton = new JButton("Atras");
+				btnNewButton.setBounds(20, 28, 110, 35);
+				btnNewButton.setBackground(Color.RED);
+				btnNewButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						
+					}
+				});
+				contentPane.add(btnNewButton);
 		
 	}
 }
